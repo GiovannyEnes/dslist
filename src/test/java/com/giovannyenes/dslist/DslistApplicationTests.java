4fefe8189f35bd4982576d7da63766ac5d7d49e9
GiovannyEnes <163896641+GiovannyEnes@users.noreply.github.com>
2025-05-21 16:34:53 -0300

    Project Created

---
+++ src/test/java/com/giovannyenes/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovannyenes.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
