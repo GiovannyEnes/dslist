e61e266e203c1226de06e4c1496da80351810075
GiovannyEnes <163896641+GiovannyEnes@users.noreply.github.com>
2025-05-22 16:56:52 -0300

    Get game by id

--- src/main/java/com/giovannyenes/dslist/controllers/GameController.java
+++ src/main/java/com/giovannyenes/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.giovannyenes.dslist.dto.GameDTO;
 import com.giovannyenes.dslist.dto.GameMinDTO;
 import com.giovannyenes.dslist.services.GameService;
 
@@ -25,5 +27,12 @@ public class GameController {
 		return result;
 		
 	}
+	
+	@GetMapping(value ="/{id}")
+	public GameDTO findByID(@PathVariable Long id){
+		return gameService.findById(id);
+		
+	}
+	
 
 }
